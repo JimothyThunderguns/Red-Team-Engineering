21c87ce3776a65c8744f2a45a0127bd00c90b7e9
Ryan Jackson <rcjackson96@gmail.com>
2022-11-09 21:09:45 -0600

    tests init

---
+++ tests/employee.test.js
@@ -0,0 +1,7 @@
+const { default: test } = require("node:test");
+
+const employee =("../lib/employee");
+//new employee object
+test('creates an employee object ', () => {
+    const Employee = new 
+})
---
+++ tests/manager.test.js
@@ -0,0 +1,9 @@
+const manager = require("../lib/manager");
+
+test("Can set office number via constructor argument", () => {
+    const testValue = 100;
+    const e = new Manager("Foo", 1, "test@test.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
+  });
+
+  
